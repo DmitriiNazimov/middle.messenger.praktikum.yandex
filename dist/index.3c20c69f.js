fb2d7670bfdbea7551cf11ed07ea2efdeefe540f
DmitriiNazimov <kurs-ssu@mail.ru>
2022-09-15 18:08:12 +0300

    Delete cache

--- dist/index.3c20c69f.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#root").textContent=function(...t){if(0===t.length)throw Error("sum required at least 1 argument");return t.reduce(((t,e)=>t+e),0)}(14,-4).toString();
-//# sourceMappingURL=index.3c20c69f.js.map
